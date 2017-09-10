9c79006b1d8dd92b35f23475700b2ba23dad09df
AJ Frank <alexanderjfrank@gmail.com>
2017-09-10 14:22:44 -0400

    allalbums

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -1,55 +1,49 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import AllAlbums from './AllAlbums';
-import SingleAlbum from './SingleAlbum';
-import Sidebar from './Sidebar';
-import Player from './Player';
+import React, { Component } from "react";
+import axios from "axios";
+import AllAlbums from "./AllAlbums";
+import SingleAlbum from "./SingleAlbum";
+import Sidebar from "./Sidebar";
+import Player from "./Player";
+import { HashRouter, Route } from "react-router-dom";
 
 export default class Main extends Component {
-
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      albums: [],
       selectedAlbum: {}
     };
     this.selectAlbum = this.selectAlbum.bind(this);
     this.deselectAlbum = this.deselectAlbum.bind(this);
   }
 
-  componentDidMount () {
-    axios.get('/api/albums/')
+  selectAlbum(albumId) {
+    axios
+      .get(`/api/albums/${albumId}`)
       .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
+      .then(album =>
+        this.setState({
+          selectedAlbum: album
+        })
+      );
   }
 
-  selectAlbum (albumId) {
-    axios.get(`/api/albums/${albumId}`)
-      .then(res => res.data)
-      .then(album => this.setState({
-        selectedAlbum: album
-      }));
+  deselectAlbum() {
+    this.setState({ selectedAlbum: {} });
   }
 
-  deselectAlbum () {
-    this.setState({ selectedAlbum: {}});
-  }
-
-  render () {
+  render() {
     return (
       <div id="main" className="container-fluid">
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
-        <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
-        </div>
+        <HashRouter>
+          <div className="col-xs-10">
+            <SingleAlbum album={this.state.selectedAlbum} />
+            <Route exact path="/" component={AllAlbums} />
+            <Route exact path="/albums" render={() => <AllAlbums />} />
+          </div>
+        </HashRouter>
         <Player />
       </div>
     );
